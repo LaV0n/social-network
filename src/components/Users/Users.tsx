388cc98fd3c9d51ad9bfd4a3a8cebb53dc58b235
LaVon <leonidmedica@gmail.com>
2022-07-26 12:21:19 +0200

    add life cycle

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -4,7 +4,7 @@ import styles from './Users.module.css'
 import axios from "axios";
 import userPhoto from '../../assets/img/userPhoto.png';
 
- type UserType = {
+type UserType = {
     users: UsersType[]
     follow: (userId: number) => void
     unfollow: (userId: number) => void
@@ -24,51 +24,49 @@ import userPhoto from '../../assets/img/userPhoto.png';
 
 
 class Users extends React.Component<UserType> {
-    constructor(props:UserType) {
-        super(props);
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
-            this.props.setUsers(response.data.items)
-        });
+                this.props.setUsers(response.data.items)
+            });
     }
-
-render() {
-    return (
-        <div>
-            {
-                this.props.users.map(u =>
-                    <div key={u.id} className={styles.user}>
-                        <div className={styles.face}>
-                            <div>
-                                <img className={styles.avatar}
-                                     src={u.photos.small !== null ? u.photos.small : userPhoto} alt="0"/>
+    render() {
+        return (
+            <div>
+                {
+                    this.props.users.map(u =>
+                        <div key={u.id} className={styles.user}>
+                            <div className={styles.face}>
+                                <div>
+                                    <img className={styles.avatar}
+                                         src={u.photos.small !== null ? u.photos.small : userPhoto} alt="0"/>
+                                </div>
+                                <div>
+                                    {u.followed
+                                        ? <button className={styles.button} onClick={() => {
+                                            this.props.unfollow(u.id)
+                                        }}>unfollow</button>
+                                        : <button className={styles.button} onClick={() => {
+                                            this.props.follow(u.id)
+                                        }}>follow</button>}
+                                </div>
                             </div>
-                            <div>
-                                {u.followed
-                                    ? <button className={styles.button} onClick={() => {
-                                        this.props.unfollow(u.id)
-                                    }}>unfollow</button>
-                                    : <button className={styles.button} onClick={() => {
-                                        this.props.follow(u.id)
-                                    }}>follow</button>}
+                            <div className={styles.description}>
+                                <div>
+                                    <div className={styles.line}><span>Name: </span>{u.name}</div>
+                                    <div className={styles.line}><span>Status: </span>{u.status}</div>
+                                </div>
+                                <div>
+                                    <div className={styles.line}><span>Country: </span>{"u.location.country"}</div>
+                                    <div className={styles.line}><span>City: </span>{"u.location.city"}</div>
+                                </div>
                             </div>
                         </div>
-                        <div className={styles.description}>
-                            <div>
-                                <div className={styles.line}><span>Name: </span>{u.name}</div>
-                                <div className={styles.line}><span>Status: </span>{u.status}</div>
-                            </div>
-                            <div>
-                                <div className={styles.line}><span>Country: </span>{"u.location.country"}</div>
-                                <div className={styles.line}><span>City: </span>{"u.location.city"}</div>
-                            </div>
-                        </div>
-                    </div>
-                )
-            }
-        </div>
-    )
-}
+                    )
+                }
+            </div>
+        )
+    }
 
 }
 
--- src/redux/UsersReducer.tsx
+++ src/redux/UsersReducer.tsx
@@ -37,7 +37,7 @@ const UsersReducer = (state:UsersPageType = initialState, action: ActionsType):U
         case 'UNFOLLOW':
             return {...state,users:state.users.map(u=>u.id===action.userId?{...u,followed:false}:u)}
         case 'SET_USERS':
-            return {...state,users:[...state.users, ...action.users]}
+            return {...state,users:[...state.users, ...action.users]} // duplicate users from server
         default:
             return state;
     }
