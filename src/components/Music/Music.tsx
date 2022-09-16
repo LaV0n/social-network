4f52515cee84e4557e1c3a3ff2e167f7539e1e36
LaVon <leonidmedica@gmail.com>
2022-09-16 13:23:22 +0200

    add music example

--- src/components/Music/Music.tsx
+++ src/components/Music/Music.tsx
@@ -4,7 +4,8 @@ import styles from "./Music.module.css";
 export const Music =  () => {
     return(
         <div className={styles.content}>
-            no music yet
+            Music example
+            <audio src={'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3'} controls/>
         </div>
     )
 }
